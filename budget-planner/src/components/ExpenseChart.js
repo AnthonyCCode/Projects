cff614222a441d3af358e75a4ef51f4bd23bc021
Anthony Calderone <anthony.calderone2000@yahoo.com>
2025-02-04 11:57:26 -0500

    BudgetPlanner

---
+++ budget-planner/src/components/ExpenseChart.js
@@ -0,0 +1,53 @@
+// import { Bar } from "react-chartjs-2";
+// import {
+//   Chart as ChartJS,
+//   Title,
+//   Tooltip,
+//   Legend,
+//   BarElement,
+//   CategoryScale,
+//   LinearScale,
+// } from "chart.js";
+
+// ChartJS.register(
+//   Title,
+//   Tooltip,
+//   Legend,
+//   BarElement,
+//   CategoryScale,
+//   LinearScale
+// );
+
+// const ExpenseChart = ({ data }) => {
+//     const chartData = {
+//       labels: ['Expense'], 
+//       datasets: [
+//         {
+//           label: 'Amount',
+//           data: [data], 
+//           backgroundColor: 'rgba(75, 192, 192, 0.2)',
+//           borderColor: 'rgba(75, 192, 192, 1)',
+//           borderWidth: .5,
+//         },
+//       ],
+//     };
+
+
+// const options = {
+//     responsive: true,
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   };
+
+//   return (
+//     <div>
+//       <h2 className="text-xl font-bold mb-4">Expense Chart</h2>
+//       <Bar data={chartData} options={options} />
+//     </div>
+//   );
+// };
+
+// export default ExpenseChart;
---
+++ budget-planner/src/components/InputField.js
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import "../styles/InputField.css";
+
+const InputField = () => {
+  const [date, setDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+  const [balance, setBalance] = useState(0);
+  const [enteredBalance, setEnteredBalance] = useState("");
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+     if (savedTheme) {
+      setTheme(savedTheme)
+      document.documentElement.setAttribute("data-theme", savedTheme);
+     }
+  }, []);
+
+  const handleChangeDate = (event) => {
+    setDate(event.target.value);
+  };
+
+  const handleChangeCategory = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleChangeAmount = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const handleChangeBalance = (event) => {
+    setEnteredBalance(event.target.value);
+  };
+
+  //handles the submit of the states
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newBalance = balance + Number(enteredBalance) - Number(amount);
+    setBalance(newBalance);
+
+    console.log("Balance Entered:", enteredBalance);
+    console.log("Category Entered:", category);
+    console.log("Amount Entered", amount);
+    console.log("Date Entered:", date);
+
+    setEnteredBalance("");
+    setCategory("");
+    setAmount("");
+    setDate("");
+  };
+
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  return (
+    <div>
+      <div className="toggle-switch">
+        <label className="switch-label">
+          <input 
+          type="checkbox" 
+          className="checkbox" 
+          checked={theme === "dark"}
+          onChange={toggleTheme}
+          />
+          <span className="slider"></span>
+        </label>
+      </div>
+      <form onSubmit={handleSubmit} className="form-container">
+        <h1 className="form-title">Balance</h1>
+        <p className="form-balance">${balance}</p>
+        <input
+          type="number"
+          value={enteredBalance}
+          onChange={handleChangeBalance}
+          placeholder="Enter your Balance"
+          className="input-field"
+        />
+        <select
+          value={category}
+          onChange={handleChangeCategory}
+          className="input-field-list"
+        >
+          <option value="">Category</option>
+          <option value="Food">Food</option>
+          <option value="Transport">Transport</option>
+          <option value="Bills">Bills</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Other">Other</option>
+        </select>
+        <input
+          type="number"
+          value={amount}
+          onChange={handleChangeAmount}
+          placeholder="Expense Amount"
+          className="input-field"
+        />
+        <input
+          type="date"
+          value={date}
+          onChange={handleChangeDate}
+          className="input-field"
+        />
+        <button type="submit" className="submit-button">
+          <span className="label">Add New Item</span>
+          <span className="gradient-container">
+            <span className="gradient"></span>
+          </span>
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default InputField;
