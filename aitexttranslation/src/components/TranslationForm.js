b911401b4afc41ffa7e4cf5cb0a6b9f1a500f3f6
Anthony Calderone <anthony.calderone2000@yahoo.com>
2025-02-17 19:34:24 -0500

    edit

--- aitexttranslation/src/components/TranslationForm.js
+++ aitexttranslation/src/components/TranslationForm.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import axios from "axios";
 import styles from "./TranslationForm.module.css";
 
-
 const TranslationForm = () => {
   const [text, setText] = useState("");
   const [targetLang, setTargetLang] = useState("fr");
@@ -20,31 +19,38 @@ const TranslationForm = () => {
     }
   };
 
+  const setLang = (event) => {
+    setTargetLang(event.target.value);
+  };
+
+  const setTargetText = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className={styles.container}>
-        <h2 className={styles.heading}>AI-Powered Translator</h2>
-        <textarea
+      <h2 className={styles.heading}>AI-Powered Translator</h2>
+      <textarea
         className={styles.textArea}
         placeholder="Enter Text..."
         value={text}
-        onChange={(e) => setText(e.target.value)}
-        />
-        <select
+        onChange={setTargetText}
+      />
+      <select
         className={styles.selectBox}
         value={targetLang}
-        onChange={(e) => setTargetLang(e.target.value)}
-        >
-            <option value="fr">French</option>
-            <option value="es">Spanish</option>
-            <option value="de">German</option>
-            <option value="zh-cn">Chinese</option>
-        </select>
-        <button className={styles.button} onClick={handleTranslate}> 
-            Translate
-        </button>
-        {translatedText && (
-            <p className={styles.translationOutput}>{translatedText}</p>
-        )}
+        onChange={setLang}
+      >
+        <option value="fr">French</option>
+        <option value="es">Spanish</option>
+        <option value="de">German</option>
+      </select>
+      <button className={styles.button} onClick={handleTranslate}>
+        Translate
+      </button>
+      {translatedText && (
+        <p className={styles.translationOutput}>{translatedText}</p>
+      )}
     </div>
   );
 };
